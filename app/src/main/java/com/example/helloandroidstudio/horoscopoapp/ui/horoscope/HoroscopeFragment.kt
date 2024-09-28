33146769b41801b3ce74e727c724f6c38d2c2da3
DFVasco <danielvasco.0112@gmail.com>
2024-09-27 19:32:39 -0500

    Advances of HoroscopoApp 12 Signos Add

--- app/src/main/java/com/example/helloandroidstudio/horoscopoapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/example/helloandroidstudio/horoscopoapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,18 +1,59 @@
 package com.example.helloandroidstudio.horoscopoapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.helloandroidstudio.databinding.FragmentHoroscopeBinding
+import com.example.helloandroidstudio.horoscopoapp.ui.horoscope.adapter.HoroscopeAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
 
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var horoscopeAdapter: HoroscopeAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initRecyclerView()
+        initUIState()
+    }
+
+    private fun initRecyclerView() {
+        horoscopeAdapter = HoroscopeAdapter()
+
+        binding.rvHoroscope.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = horoscopeAdapter
+        }
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect {
+                    Log.i("Daniel", it.toString())
+                    horoscopeAdapter.updateList(it)
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
---
+++ app/src/main/java/com/example/helloandroidstudio/horoscopoapp/ui/horoscope/HoroscopeViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.helloandroidstudio.horoscopoapp.ui.horoscope
+
+import androidx.lifecycle.ViewModel
+import com.example.helloandroidstudio.horoscopoapp.data.providers.HoroscopeProvider
+import com.example.helloandroidstudio.horoscopoapp.domain.model.HoroscopeInfo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class HoroscopeViewModel @Inject constructor(horoscopeProvider: HoroscopeProvider) : ViewModel() {
+
+    private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope: StateFlow<List<HoroscopeInfo>> = _horoscope
+
+    init {
+        _horoscope.value = horoscopeProvider.getHoroscope()
+
+    }
+
+}
